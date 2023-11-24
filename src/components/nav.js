d4ceed5f2f34bcf8f21a0c9f24f72c9352e2d92f
Johan Nilsson <johan.nilsson.mail@gmail.com>
2023-11-24 17:45:03 +0100

    Initial commit

---
+++ src/components/nav.js
@@ -0,0 +1,27 @@
+import '../styles/nav.scss';
+import { NavLink } from 'react-router-dom';
+
+const Nav = () => {
+  return (
+    <header className='header'>
+      <nav className='navbar'>
+        <NavLink
+          to='/'
+          className={({ isActive, isPending }) =>
+            isPending ? 'pending' : isActive ? 'active' : ''
+          }>
+          Accueil
+        </NavLink>
+        <NavLink
+          to='/a-propos'
+          className={({ isActive, isPending }) =>
+            isPending ? 'pending' : isActive ? 'active' : ''
+          }>
+          À Propos
+        </NavLink>
+      </nav>
+    </header>
+  );
+};
+
+export default Nav;
