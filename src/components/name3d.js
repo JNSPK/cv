cec8285dd76566cf9e3392d726b2d0612741f57b
Johan Nilsson <johan.nilsson.mail@gmail.com>
2023-12-12 04:43:39 +0100

    feature: Habillage 3d

--- src/components/name3d.js
+++ src/components/name3d.js
@@ -1,6 +1,13 @@
 import { Canvas, useFrame, useThree } from '@react-three/fiber';
-import { Center, Text3D, Float } from '@react-three/drei';
+import {
+  Center,
+  Text3D,
+  Float,
+  useTexture,
+  Environment,
+} from '@react-three/drei';
 import { useRef } from 'react';
+import { PointLight } from 'three';
 
 const CustomMesh = () => {
   const ref = useRef();
@@ -15,95 +22,116 @@ const CustomMesh = () => {
   });
 
   return (
-    <Center>
-      <Float
-        speed={3} // Animation speed, defaults to 1
-        rotationIntensity={0.8} // XYZ rotation intensity, defaults to 1
-        floatIntensity={0.3} // Up/down float intensity, works like a multiplier with floatingRange,defaults to 1
-        floatingRange={[1, 6]} // Range of y-axis values the object will float within, defaults to [-0.1,0.1]
-      >
-        <group ref={ref}>
-          <mesh position={[5.5, -2, 2.5]}>
-            <Text3D
-              bevelSegments={20}
-              bevelThickness={0.5}
-              bevelEnabled
-              curveSegments={20}
-              size={1}
-              lineHeight={0.5}
-              letterSpacing={0.1}
-              font={'/Payback.json'}>
-              {`Portfolio`}
-              <meshStandardMaterial color={'#E0FBFC'} />
-            </Text3D>
-          </mesh>
-          <mesh position={[-2, 0, 3]}>
-            <Text3D
-              bevelSegments={20}
-              bevelThickness={1}
-              bevelEnabled
-              curveSegments={20}
-              size={3.5}
-              lineHeight={1}
-              letterSpacing={0.1}
-              font={'/BLASTIMO.json'}>
-              {`Johan Nilsson`}
-              <meshStandardMaterial color={'#E0FBFC'} />
-            </Text3D>
-          </mesh>
-        </group>
-      </Float>
-    </Center>
+    <>
+      {' '}
+      <Center>
+        <group ref={ref} position={(-8, -10, 2)}>
+          <Float
+            speed={2} // Animation speed, defaults to 1
+            rotationIntensity={0.8} // XYZ rotation intensity, defaults to 1
+            floatIntensity={0.1} // Up/down float intensity, works like a multiplier with floatingRange,defaults to 1
+            floatingRange={[2, 1.5]} // Range of y-axis values the object will float within, defaults to [-0.1,0.1]
+          >
+            <mesh>
+              <Text3D
+                bevelSegments={20}
+                bevelThickness={1}
+                bevelEnabled
+                curveSegments={20}
+                size={3.5}
+                lineHeight={1}
+                letterSpacing={0.1}
+                font={'/BLASTIMO.json'}>
+                {`Johan Nilsson`}
+                <meshStandardMaterial />
+              </Text3D>
+            </mesh>
+            <mesh position={[5.5, -2, 1.5]}>
+              <Text3D
+                bevelSegments={20}
+                bevelThickness={0.5}
+                bevelEnabled
+                curveSegments={20}
+                size={1}
+                lineHeight={0.5}
+                letterSpacing={0.1}
+                font={'/Payback.json'}>
+                {`Portfolio`}
+                <meshStandardMaterial />
+              </Text3D>
+            </mesh>
+          </Float>
+        </group>{' '}
+      </Center>
+    </>
   );
 };
 
 const Scene = () => {
-  const directionalLightRef = useRef();
   const spotLightref = useRef();
   const spotLightref2 = useRef();
 
-  const lightColour = '#ffb649';
-  const lightIntensity = 3.1;
-  const spotLightColour = '#ffbfd2';
-  const spotLightIntensity = 1.7;
+  const lightColour = '#ffb649'; //orange
+  const spotLightIntensity = 1.3;
   const spotLightRadius = 90;
-  const spotLightColour2 = '#739000';
-  const spotLightIntensity2 = 1.8;
+  const spotLightColour2 = '#4C3B4D'; //violet
+  const spotLightIntensity2 = 8;
   const spotLightRadius2 = 130;
+  const texture = useTexture('./textures/marble.jpg');
+  const texture2 = useTexture('./textures/marble2.jpg');
 
   return (
     <>
+      {/* <Environment preset='dawn' /> */}
+      <ambientLight intensity={3} color={spotLightColour2} />
+
       <spotLight
-        position={[5, 2, 15]}
+        position={[-2, 2, -3]}
         ref={spotLightref}
         intensity={spotLightIntensity}
-        color={spotLightColour}
+        color={lightColour} //orange
         angle={spotLightRadius}
         decay={0}
+        map={texture}
       />
       <spotLight
-        position={[-5, -10, 10]}
+        position={[-5, -10, 0]}
         ref={spotLightref2}
         intensity={spotLightIntensity2}
-        color={spotLightColour2}
+        color={spotLightColour2} //violet
         angle={spotLightRadius2}
         decay={0}
+        map={texture}
       />
-      <directionalLight
-        position={[-2, 3, 3]}
-        intensity={lightIntensity}
-        ref={directionalLightRef}
-        color={lightColour}
+
+      <spotLight
+        position={[2, 2, -2]}
+        ref={spotLightref}
+        intensity={1}
+        color={'red'} //rouge
+        angle={spotLightRadius}
+        decay={0}
+        map={texture2}
       />
-      <directionalLight position={[0, 0, 5]} intensity={3.5} color={'white'} />
+
+      <spotLight
+        position={(10, 0, 7)}
+        ref={spotLightref2}
+        intensity={3}
+        angle={1}
+        decay={0}
+        map={texture}
+      />
+
       <CustomMesh />
     </>
   );
 };
+
 const Name3d = () => {
   return (
     <>
-      <Canvas camera={{ position: [-3, -1, 12] }}>
+      <Canvas orthographic camera={{ position: [0, 0, 10], zoom: 50 }}>
         <Scene />
       </Canvas>
     </>
